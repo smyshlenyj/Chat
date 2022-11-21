b1d973477545545ba300b3c6ff0bf3afca60e433
J <smyshlenyj@gmail.com>
2022-11-21 20:12:43 +0300

    + add menu for main functions

--- Chat/Chat.cpp
+++ Chat/Chat.cpp
@@ -150,19 +150,18 @@ struct Message
 {
 private:
 	bool groupChat;
-	int idSender, idRecipient;
-	std::string message, chatName;
+	std::string sender, recipient, message, chatName;
 
 public:
-	Message(int _idSender, int _idRecipient, std::string _message) : idSender(_idSender), idRecipient(_idRecipient), message(_message)
+	Message(std::string _sender, std::string _recipient, std::string _message) : sender(_sender), recipient(_recipient), message(_message)
 	{
 		groupChat = false;
 		chatName = "-";
 	}
 
-	Message(bool _groupChat, std::string _chatName, int _idSender, std::string _message) : groupChat(_groupChat), chatName(_chatName), idSender(_idSender), message(_message)
+	Message(bool _groupChat, std::string _chatName, std::string _sender, std::string _message) : groupChat(_groupChat), chatName(_chatName), sender(_sender), message(_message)
 	{
-		idRecipient = 0; //need to reserve 0 for public chat
+		recipient = "_all"; //need to reserve name for public chat
 	}
 
 	void sendMessage() // push message to data base
@@ -170,7 +169,7 @@ public:
 		std::ofstream out("messages.mdf", std::ios::app);
 		if (out.is_open())
 		{
-			out << chatName << "$" << groupChat << "$" << idSender << "$" << idRecipient << "$" << message << std::endl;
+			out << chatName << "$" << groupChat << "$" << sender << "$" << recipient << "$" << message << std::endl;
 		}
 		out.close();
 	}
@@ -180,7 +179,7 @@ struct Chat
 {
 	std::list<std::string> buffer;
 
-	Chat(int _id1, int _id2) // private chat constructor
+	Chat(std::string _sender, std::string _recipient) // private chat constructor
 	{
 		std::ifstream readFromDB;
 		readFromDB.open("messages.mdf", std::ios::in);
@@ -202,8 +201,8 @@ struct Chat
 					++i;
 				}
 				// showing messages linked with current users
-				if ((std::stoi(array[2]) == _id1 && std::stoi(array[3]) == _id2) || (std::stoi(array[2]) == _id2 && std::stoi(array[3]) == _id1))
-					buffer.push_back("From: " + array[2] + " To: " + array[3] + " Message: " + array[4]);
+				if ((array[2] == _sender && array[3] == _recipient) || (array[2] == _recipient && array[3] == _sender))
+					buffer.push_back("From: " + array[2] + "\tTo: " + array[3] + "\tMessage: " + array[4]);
 			}
 		}
 	}
@@ -231,7 +230,7 @@ struct Chat
 				}
 
 				if (array[0] == _chatName) // showing messages linked with current chat
-					buffer.push_back("Public chat: " + array[0] + " From: " + array[2] + " Message: " + array[4]);
+					buffer.push_back("Public chat: " + array[0] + "\tFrom: " + array[2] + "\tMessage: " + array[4]);
 			}
 		}
 	}
@@ -260,27 +259,40 @@ int main()
 
 	int input;
 	std::string current_user;
-	bool escape = true;
-	while (escape) {
-		std::cout << "sign in, sign up, or leave: Enter '0', '1' or '2'\n";
+	bool alive = true;
+	while (alive) {
+		std::cout << "\nPress\n '1' for sign in\n '2' for sign up\n '3' for exit\n";
 		std::cin >> input;
 		switch (input) {
-		case 0: {
+		case 1: {
 			current_user = sign_in();
-			if (current_user != "") escape = false;
+			//if (current_user != "") alive = false;
+
+			std::cout << "Hi, " << current_user << ", please type recipient name or public chat name: ";
+			std::string inputRecipient;
+			std::cin >> inputRecipient;
+			std::cout << '\n' << "Last messages in chat: " << '\n';
+			Chat temp = { current_user, inputRecipient };
+			temp.print();
+			std::cout << '\n' << "Type your message here: " << '\n';
+			std::string inputMessage;
+			std::cin.ignore();
+			std::getline(std::cin, inputMessage, '\n');
+			Message tempMessage = { current_user, inputRecipient, inputMessage };
+			tempMessage.sendMessage();
 		}; break;
-		case 1: { if (sign_up()) break; }; break;
-		case 2: { escape = false; }; break;
+		case 2: { if (sign_up()) break; }; break;
+		case 3: { alive = false; }; break;
 		default: std::cout << "Invalid input\n"; break;
 		}
 	}
 
-	std::cout << '\n' << current_user << '\n';
 
-	// Message test = { 56, 65, "hello world!"};
-	// test.sendMessage();
-	// Chat ch = { 56, 65 };
-	// ch.print();
+
+	//Message test = { "test_sender", "test_recipient", "hello world!" };
+	//test.sendMessage();
+	//Chat ch = { "test_sender", "test_recipient" };
+	//ch.print();
 	// Chat common = { "00-general" };
 	// common.print();
 	// Message testGroup = { true, "00-general", 3, "public chat \"TEST message" };
