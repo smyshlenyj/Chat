a7df64dce28ec6ef6e1c00d60637367dab699341
smyshlenyj <smyshlenyj@gmail.com>
2022-11-22 13:57:32 +0300

    *

--- Chat/Chat.cpp
+++ Chat/Chat.cpp
@@ -225,66 +225,61 @@ struct Chat
 		std::ifstream readFromDB;
 		readFromDB.open("messages.mdf", std::ios::in);
 
-		if (!readFromDB.is_open())
-			std::ofstream outfile("messages.mdf"); //create file in case it's not there
-		else
-		{
-			while (!readFromDB.eof())
-			{
-				std::string array[5];
-				std::string msg;
-
-				std::getline(readFromDB, msg);
-				if (!msg.empty())
+				while (!readFromDB.eof())
 				{
-					std::istringstream ss(msg);
-					std::string token;
-					int i = 0; // iterator for while
-					while (std::getline(ss, token, '\t'))
+					std::string array[5];
+					std::string msg;
+
+					std::getline(readFromDB, msg);
+					if (!msg.empty())
 					{
-						array[i] = token;
-						++i;
+						std::istringstream ss(msg);
+						std::string token;
+						int i = 0; // iterator for while
+						while (std::getline(ss, token, '\t'))
+						{
+							array[i] = token;
+							++i;
+						}
+						// showing messages linked with current users
+						if ((array[2] == _sender && array[3] == _recipient) || (array[2] == _recipient && array[3] == _sender))
+							buffer.push_back("From: " + array[2] + "\tTo: " + array[3] + "\tMessage: " + array[4]);
 					}
-					// showing messages linked with current users
-					if ((array[2] == _sender && array[3] == _recipient) || (array[2] == _recipient && array[3] == _sender))
-						buffer.push_back("From: " + array[2] + "\tTo: " + array[3] + "\tMessage: " + array[4]);
 				}
-			}
-		}
 	}
 
-	Chat(std::string _chatName) // public chat constructor
-	{
-		std::ifstream readFromDB;
-		readFromDB.open("messages.mdf", std::ios::in);
-		std::string array[5];
+	//Chat(std::string _chatName) // public chat constructor
+	//{
+	//	std::ifstream readFromDB;
+	//	readFromDB.open("messages.mdf", std::ios::in);
+	//	std::string array[5];
 
-		if (!readFromDB.is_open())
-			std::ofstream outfile("messages.mdf"); //create file in case it's not there
-		else
-		{
-			while (!readFromDB.eof())
-			{
-				std::string msg;
-				std::getline(readFromDB, msg);
+	//	if (!readFromDB.is_open())
+	//		std::ofstream outfile("messages.mdf"); //create file in case it's not there
+	//	else
+	//	{
+	//		while (!readFromDB.eof())
+	//		{
+	//			std::string msg;
+	//			std::getline(readFromDB, msg);
 
-				if (!msg.empty())
-				{
-					std::istringstream ss(msg);
-					std::string token;
-					int i = 0; // iterator for while
-					while (std::getline(ss, token, '\t'))
-					{
-						array[i] = token;
-						++i;
-					}
+	//			if (!msg.empty())
+	//			{
+	//				std::istringstream ss(msg);
+	//				std::string token;
+	//				int i = 0; // iterator for while
+	//				while (std::getline(ss, token, '\t'))
+	//				{
+	//					array[i] = token;
+	//					++i;
+	//				}
 
-					if (array[0] == _chatName) // showing messages linked with current chat
-						buffer.push_back("Public chat: " + array[0] + "\tFrom: " + array[2] + "\tMessage: " + array[4]);
-				}
-			}
-		}
-	}
+	//				if (array[0] == _chatName) // showing messages linked with current chat
+	//					buffer.push_back("Public chat: " + array[0] + "\tFrom: " + array[2] + "\tMessage: " + array[4]);
+	//			}
+	//		}
+	//	}
+	//}
 
 	// std::list<std::string> getChat() { return buffer; } not used
 
